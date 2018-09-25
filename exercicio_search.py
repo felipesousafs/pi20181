52df41222de531bb63374fd78f1f018fec199716
Felipe Sousa <felipe.sousa.fs@gmail.com>
2018-09-25 17:03:08 -0300

    update

--- exercicio_search.py
+++ exercicio_search.py
@@ -1,15 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
+occurrences = []
+
 def search(keyword, url, depth):
+    depth -= 1
     response = requests.get(url)
     page = BeautifulSoup(response.content)
     links = page.find_all('a')
     for link in links:
-        print('Link: ', link.get('href'))
+        print('URL: ', link.get('href'))
         print('Depth: ', depth)
-        if depth > 0:
-            search(keyword, link.get('href'), depth)
+        print('Element <a>: ', link)
+        if depth > 0 and link.get('href') != None:
+            print('Find HTTP:', link.get('href').find('http'))
+            if link.get('href').find('http') > -1:
+                check_occurrence(response.text, keyword)
+                search(keyword, link.get('href'), depth)
 
-url = 'http://libra.ifpi.edu.br'
-search('ifpi', url, 0)
+def check_occurrence(content, kw):
+    number_of_ocurrences = content.count(kw)
+    indexes = [i for i, x in enumerate(content) if x == kw]
+    print('indexes: ', indexes)
+    print('number of occurrences: ', number_of_ocurrences)
+    print('=========== CONTENT ============== :', content)
+url = 'https://globoesporte.globo.com/'
+search('flamengo', url, 2)
