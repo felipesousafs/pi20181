202da26e775041221cd5a487906970765e3b3bbd
Felipe Sousa <felipe.sousa.fs@gmail.com>
2018-09-26 15:47:12 -0300

    Minor text fixes; Corrigido contagem de ocorrencias;

--- exercicio_search.py
+++ exercicio_search.py
@@ -1,30 +1,31 @@
 import requests
 import re
+import requests_cache
+import time
 from bs4 import BeautifulSoup
+
+start_time = time.time()
+
+# requests_cache.install_cache('demo_cache')
+requests_cache.uninstall_cache()
 
 occurrences = []
 
 def search(keyword, url, depth):
     depth -= 1
-    response = requests.get(url)
+    with requests_cache.disabled():
+        response = requests.get(url)
     page = BeautifulSoup(response.content, 'html5lib')
     links = page.find_all('a')
     for link in links:
-        print('URL: ', link.get('href'))
-        print('Depth: ', depth)
-        print('Element <a>: ', link)
         if link.get('href') != None:
             print('Find HTTP:', link.get('href').find('http'))
             if depth > 0 and link.get('href').find('http') > -1:
                 search(keyword, link.get('href'), depth)
-    check_occurrence(page.get_text(), keyword)
+    check_occurrence(response.text, keyword)
 
 def check_occurrence(content, kw):
-    number_of_ocurrences = content.lower().count(kw)
-    indexes = [m.start() for m in re.finditer(kw.lower(), content.lower())]
-    print('Indexes: ', indexes)
-    print('kw: ', kw)
-    print('number of occurrences: ', number_of_ocurrences)
+    indexes = [i.start() for i in re.finditer(kw.lower(), content.lower())]
     for index in indexes:
         occurrences.append(content[index-30:index+30])
 
@@ -33,7 +34,17 @@ def print_each_occurrence(occurrences):
         print(occurrence)
 
 
-url = 'https://globoesporte.globo.com/'
-search('flamengo', url, 2)
+url_ge = 'https://globoesporte.globo.com/'
+keyword_ge = 'flamengo'
+url_americanas = 'https://www.americanas.com.br/'
+keyword_americanas = 'geladeira'
+url_rege = 'http://regeneracao.pi.gov.br/'
+keyword_rege = 'prefeitura'
+search(keyword_americanas, url_americanas, 1)
 print('============ Ocorrências ============')
 print_each_occurrence(occurrences)
+print('------------------------------------')
+print('Number of occurrences: ', occurrences.__len__())
+print('Keyword: ', keyword_americanas)
+print('URL: ', url_americanas)
+print("--- Execution time: %s seconds ---" % (time.time() - start_time))
